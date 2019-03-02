da8429e73ba86437a81959a4b317ddadcc87ed1b
Boyan Kushlev <takarabukara@gmail.com>
2019-03-02 12:50:42 +0200

    rename to Hippo Survey

--- src/app/survey/submission-page/submission-form/submission-form.component.ts
+++ src/app/survey/submission-page/submission-form/submission-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Question } from '../model/question';
+import { Question } from '../../model/question';
 import { FormGroup } from '@angular/forms';
-import { QuestionControlService } from '../services/question-control.service';
-import { QuestionService } from '../services/question.service';
+import { QuestionControlService } from '../../services/question-control.service';
+import { QuestionService } from '../../services/question.service';
 import { Observable } from 'rxjs';
 
 @Component({

76882fa36f76fa77ec778b417c557702b9d43dfb
Boyan Kushlev <takarabukara@gmail.com>
2019-03-03 14:33:25 +0200

    assemble and post submission object

--- src/app/survey/model/submission.ts
+++ src/app/survey/model/submission.ts
@@ -2,10 +2,16 @@ import { User } from '../../user/model/user';
 import { Survey } from './survey';
 import { AnsweredQuestion } from './answered-question';
 
-export interface Submission {
+export class Submission {
   _id: string;
   timestamp: number;
   user: User;
   survey: Survey;
   answers: AnsweredQuestion[];
+
+  constructor(user, survey, answers: AnsweredQuestion[]) {
+    this.user = user;
+    this.survey = survey;
+    this.answers = answers;
+  }
 }

3b53b7086c25a1b1390ad089b4ee1305a7ad0dcd
Boyan Kushlev <takarabukara@gmail.com>
2019-06-25 23:57:23 +0300

    fix backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   appUrl: 'http://localhost:80',
-  coreSurveyServiceUrl: 'https://hippo-survey-core.herokuapp.com/'
+  coreSurveyServiceUrl: 'https://hippo-survey-core.herokuapp.com'
 };
